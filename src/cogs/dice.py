3d464ebdaa5f44e1227212ea5aa560e73d313ed4
swatch neetend <curest.dgz@gmail.com>
2021-06-10 18:34:46 +0900

    make enable to calc with dice 2

--- src/cogs/dice.py
+++ src/cogs/dice.py
@@ -15,50 +15,50 @@ class Dice(commands.Cog):
         self.raw = s.replace(" ", "")
         self.script = self.raw
         self.result = 0
-        
+
         self.calc(s)
 
         await ctx.send("s -> (" + self.script + ") -> " + str(self.result))
 
     @commands.command(name="dice", aliases=["d"])
-    def calc(self,s):
+    def calc(self, s):
         """dices"""
         ret = 0
-        if "-" in s :
+        if "-" in s:
             a = s.split("-")
             ret += self.calc(a[0])
-            for item in a[1:] :
+            for item in a[1:]:
                 ret -= self.calc(item)
             return ret
-        if "+" in s :
+        if "+" in s:
             a = s.split("+")
-            for item in a :
+            for item in a:
                 ret += self.term(item)
             return ret
         return self.term(s)
-            
+
     # 式を部分で処理する関数
     # 乗法と除法を処理
-    def term(self,s):
+    def term(self, s):
         ret = 1
-        if "*" in s :
+        if "*" in s:
             a = s.split("*")
-            for item in a :
+            for item in a:
                 ret *= self.term(item)
             return ret
-        if "/" in s :
+        if "/" in s:
             a = s.split("/")
-            for item in a :
+            for item in a:
                 ret /= self.term(item)
             return ret
-        if "d" in s :
+        if "d" in s:
             return self.dice(s)
         return int(s)
 
     # ダイスを振る関数
     # ダイス部分を処理
-    def dice(self,s):
-        dicenum , sidenum = s.split("d")
+    def dice(self, s):
+        dicenum, sidenum = s.split("d")
         ret = 0
         prscript = []
         for i in range(int(dicenum)):
@@ -66,7 +66,7 @@ class Dice(commands.Cog):
             prscript.append(a)
             ret += a
             prscript = [str(i) for i in prscript]
-            self.script = self.script.replace(s, "("+",".join(prscript)+")",1)
+            self.script = self.script.replace(s, "(" + ",".join(prscript) + ")", 1)
         return ret
 
 
